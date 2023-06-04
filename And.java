e31194159213ccb617a12ed4ca4e0ba611569094
kostasloukanaris <135523833+kostasloukanaris@users.noreply.github.com>
2023-06-04 17:35:11 +0300

    Delete And.java

--- And.java
+++
@@ -1,9 +0,0 @@
-public class And extends Gate{
-    public And(boolean in1, boolean in2){
-        super(in1,in2);
-    }
-    public And(){this(false,false);}
-    public boolean calculateOutput(){
-       return out=in[0]&&in[1];
-    }
-}
