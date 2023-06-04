59144f7045b71b02da99d72736e390280473e6d1
kostasloukanaris <135523833+kostasloukanaris@users.noreply.github.com>
2023-06-04 17:36:07 +0300

    Delete Nand.java

--- Nand.java
+++
@@ -1,9 +0,0 @@
-public class Nand extends Gate{
-    public Nand(boolean in1, boolean in2){
-        super(in1,in2);
-    }
-    public Nand(){this(false,false);}
-    public boolean calculateOutput(){
-        return out=!(in[0]&&in[1]);
-    }
-}
